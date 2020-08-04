6df8ff626fbf21a7e84a2eb266b7e6f40653aa4b
Roberta <luz3luce@gmail.com>
2020-08-05 00:59:02 +0300

    Updates

---
+++ precache-manifest.94b5950993212e168cde5f067d02aa70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29fecacb17caacedd25d74c070b97415",
+    "url": "/Notepad/index.html"
+  },
+  {
+    "revision": "441828f93d7191c5b988",
+    "url": "/Notepad/static/js/2.7117dd17.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/Notepad/static/js/2.7117dd17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "738c64a2cc9343fb52fa",
+    "url": "/Notepad/static/js/main.b37320d0.chunk.js"
+  },
+  {
+    "revision": "a129818fcc0b5c8235eb",
+    "url": "/Notepad/static/js/runtime-main.8f0382c5.js"
+  }
+]);
